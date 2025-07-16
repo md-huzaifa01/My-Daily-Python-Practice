d681c13a8f06a2216925736dc5f0c8e76a6baf44
MD Huzaifa <hmd131021@gmail.com>
2025-07-17 05:57:41 +0600

    Add files via upload

---
+++ Week 1/Multplication-Addition-Division-Subtraction with two number.py
@@ -0,0 +1,6 @@
+num1 = 194
+num2 = 69
+print("The Subtraction fo this two number is:" , num1 - num2)
+print("The Addition of this two number is:", num1 + num2)
+print("The Multiplication of this two number is:", num1 * num2)
+print("The Division of this two number is:", num1/num2)
