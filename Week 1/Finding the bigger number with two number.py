c51ef8cf9db2a6add860c0e39a6e2d9201b88882
MD Huzaifa <hmd131021@gmail.com>
2025-07-18 00:10:59 +0600

    Add files via upload

---
+++ Week 1/Finding the bigger number with two number.py
@@ -0,0 +1,8 @@
+num1=int(input("Enter the first number: "))
+num2=int(input("Enter the second number: "))
+if num1>num2:
+    print("The bigger number is ", num1)
+elif num2>num1:
+    print("The bigger number is ", num2)
+else:
+    print("Both number are equal.")
