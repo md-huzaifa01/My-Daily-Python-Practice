58ccfb5bc9bc0485c9026b12741f9ca6c6884389
MD Huzaifa <hmd131021@gmail.com>
2025-07-18 00:21:22 +0600

    Update Finding bigger number within 3 number.py

--- Week 1/Finding bigger number within 3 number.py
+++ Week 1/Finding bigger number within 3 number.py
@@ -2,8 +2,8 @@ num1=int(input("Enter the first number: "))
 num2=int(input("Enter the second number: "))
 num3=int(input("Enter the third number: "))
 bigger= num1
-if (num2>bigger):
+if (num2 > bigger):
     bigger=num2
-if (num3>bigger):
+if (num3 > bigger):
     bigger=num3
-print("The max number is", bigger)
+print("The max number is: ", bigger)
