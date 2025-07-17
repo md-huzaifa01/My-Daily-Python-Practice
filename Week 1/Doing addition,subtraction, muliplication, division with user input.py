e83793610363db4d5e76eff6e841455a531ef8cb
MD Huzaifa <hmd131021@gmail.com>
2025-07-17 23:09:35 +0600

    Add files via upload

---
+++ Week 1/Doing addition,subtraction, muliplication, division with user input.py
@@ -0,0 +1,6 @@
+num1=int(input("Enter the first number:"))
+num2=int(input("Enter the second number: "))
+print("The addition is :", num1+num2)
+print("The subtraction is :", num1-num2)
+print("The multiplication is: ", num1*num2)
+print("The division is: ", num1/num2)
