3ae724b8538ea51ca5996d69e05735ab4a5f83a2
MD Huzaifa <hmd131021@gmail.com>
2025-07-23 20:21:35 +0600

    Add files via upload

---
+++ Week 1/Finding Negetive or Postitive Number.py
@@ -0,0 +1,7 @@
+num=float(input("Enter a number: "))
+if num>0:
+    print("Your entered number is positive.")
+elif num==0:
+    print("Yuor entered number is zero.")
+else:
+    print("Your entered number is negative.")
