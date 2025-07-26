266b35eff5fb99246bbdcabce1fc269d818759fc
MD Huzaifa <hmd131021@gmail.com>
2025-07-26 23:50:42 +0600

    Add files via upload

---
+++ Week 2/Swaping Programme.py
@@ -0,0 +1,7 @@
+a=int(input("Enter the value of a: "))
+b=int(input("Enter the value of b: "))
+temp=a
+a=b
+b=temp
+print("The value of a after swapping: {}".format(a))
+print("The value of b after swapping: {}".format(b))
