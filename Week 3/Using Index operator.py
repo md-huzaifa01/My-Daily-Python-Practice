6fbabe6351465e9a6417311348c4d3f869bae5d9
MD Huzaifa <hmd131021@gmail.com>
2025-08-01 00:43:32 +0600

    Add files via upload

---
+++ Week 3/Using Index operator.py
@@ -0,0 +1,14 @@
+name = 'Mehedi Rifa'
+letter = name[0]
+letter2= name[1]
+letter3= name[2]
+letter4= name[3]
+letter5= name[4]
+letter6= name[5]
+letter7= name[7]
+letter8= name[8]
+letter9= name[9]
+letter10= name[10]
+all_letter= len(name)
+print(letter,letter2,letter3,letter4,letter5,letter6 + " + " + letter7,letter8,letter9,letter10)
+print("The Character is: ", all_letter)
