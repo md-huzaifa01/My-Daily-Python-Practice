1377a88fd1cdcdaa08e3125ad347ed4e96e5d3b0
MD Huzaifa <hmd131021@gmail.com>
2025-08-02 23:43:49 +0600

    Add files via upload

---
+++ Week 3/Finding the area of square and perimeter of square.py
@@ -0,0 +1,7 @@
+# Formula: Area of square = Side * Side
+# Perimeter of Square= 4* Side
+side=float(input("Enter the side of the square: "))
+Squ_area= side*side
+Squ_per=4*side
+print("The area of the square is: %.4f" %Squ_area)
+print("The perimeter of the square is: %.4f" %Squ_area)
