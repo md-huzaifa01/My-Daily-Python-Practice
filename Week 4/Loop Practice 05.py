449dfbffc2af61225b54c0ed4c90b37cb7c5d6dd
MD Huzaifa <hmd131021@gmail.com>
2025-08-10 02:02:28 +0600

    Add files via upload

---
+++ Week 4/Loop Practice 05.py
@@ -0,0 +1,8 @@
+# Q7: Use a while loop to calculate and print the sum of all numbers from 1 to 100.
+num = 1
+total = 0
+
+while num <=100:
+    total = total + num
+    num = num + 1
+    print("The sum is : ", total)
