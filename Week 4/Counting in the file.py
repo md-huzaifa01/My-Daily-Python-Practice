676c16a2c4772eb9f2a7eaa644ccf08f1473f148
MD Huzaifa <hmd131021@gmail.com>
2025-08-11 03:40:55 +0600

    Add files via upload

---
+++ Week 4/Counting in the file.py
@@ -0,0 +1,5 @@
+fhand = open('data.txt') # In hare you have to a file in the same directory where your programme are running.
+count =0
+for y in fhand:
+    count = count +1
+print('The line count: ', count)
