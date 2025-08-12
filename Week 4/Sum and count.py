339a87c51cc27821774069ae67c7393b58031344
MD Huzaifa <hmd131021@gmail.com>
2025-08-12 06:04:15 +0600

    Add files via upload

---
+++ Week 4/Sum and count.py
@@ -0,0 +1,9 @@
+number = [1.5, 2.5, 6.5]
+total = 0
+count = 0
+for n in number:
+    total += n
+    count += 1
+print('The total is',total, 'and the count is ',count)
+average = total/count
+print('The average is: ', average)
