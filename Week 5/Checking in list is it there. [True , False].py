2aeaa75ec937554abd69329fa5a18c3f92164ca4
MD Huzaifa <hmd131021@gmail.com>
2025-08-15 05:44:09 +0600

    Add files via upload

---
+++ Week 5/Checking in list is it there. [True , False].py
@@ -0,0 +1,8 @@
+some = [5,4,588,5,4,5,48,9,56]
+print(5 in some)
+print(45 in some)
+print(48 in some)
+list1= ['alo', 58,99,98,25,879,94,9,98,9,'nice','good']
+print('alo' in list1)
+print(99 in list1)
+print('bad' in list1)
