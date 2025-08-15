be370994d47ffb55c07b9a548797b46c7427abee
MD Huzaifa <hmd131021@gmail.com>
2025-08-16 03:34:18 +0600

    Add files via upload

---
+++ Week 5/checking another 1.py
@@ -0,0 +1,6 @@
+list1 = ['wood', 'nice', 'like', 'good', 'true']
+print('The normal way.', list1)
+list1.sort()
+print('The sorted way.',list1)
+print('Printing the third value: ',list1 [2])
+print('Printing the forth value: ',list1[3])
