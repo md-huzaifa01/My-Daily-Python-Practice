42ac6c535a56a7760612b2ec739e3c939eb131bb
MD Huzaifa <hmd131021@gmail.com>
2025-08-16 03:54:00 +0600

    Add files via upload

---
+++ Week 5/The use of Dictionaries.py
@@ -0,0 +1,11 @@
+num_series = dict()
+num_series['Bangla']= 87
+num_series['English']= 92
+num_series['Arabic']=88
+num_series['Math'] = 95
+print(num_series)
+print('Printing the value of Bangla:',num_series['Bangla'])
+print('Printing the value of English:', num_series['English'])
+# adding in new things
+num_series['Arabic'] = num_series['Arabic'] + 5
+print(num_series)
