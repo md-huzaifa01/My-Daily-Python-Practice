7b5a4f2992e0680709641b52ca0c73dde42c351f
MD Huzaifa <hmd131021@gmail.com>
2025-08-17 01:57:18 +0600

    Add files via upload

---
+++ Week 5/Loop in Dictinory with items().py
@@ -0,0 +1,5 @@
+count = {'mehedi' : 27, 'salman' : 23, 'apil' : 22}
+for name,age in count.items():
+    print(name, age)
+for name,age in count.items():
+    print('The name', name, 'and the age is', age)
