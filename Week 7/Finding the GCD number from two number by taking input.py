57c9482c7a8146fac7b7f5d847cc57a4d8348ad5
MD Huzaifa <hmd131021@gmail.com>
2025-08-30 04:52:55 +0600

    Add files via upload

---
+++ Week 7/Finding the GCD number from two number by taking input.py
@@ -0,0 +1,8 @@
+def computeGCD(x, y):
+    while(y):
+        x, y = y, x % y
+    return x
+num1= int(input('Enter the first number: '))
+num2= int(input('Enter the second number: '))
+print('The GCD of {0} and {1} is: '.format(num1,num2), end = '')
+print(computeGCD(num1,num2))
