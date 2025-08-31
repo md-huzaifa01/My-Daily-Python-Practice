7835379d672fc1e80a2e024181cfbf8346a1733e
MD Huzaifa <hmd131021@gmail.com>
2025-08-31 23:58:25 +0600

    Update using import.py

--- Week 7/using import.py
+++ Week 7/using import.py
@@ -1,6 +1,8 @@
+# In Hare, I had done it by taking input, not using import().
 import re
 hand = open('mbox-short.txt')
 for line in hand:
     line = line.strip()
     if re.search('From: ', line):
+
         print(line)
