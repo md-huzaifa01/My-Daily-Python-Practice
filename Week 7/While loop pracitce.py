0985a12ef713eb2edbb38447c6bb6a461b1dc01f
MD Huzaifa <hmd131021@gmail.com>
2025-09-02 23:57:12 +0600

    Add files via upload

---
+++ Week 7/While loop pracitce.py
@@ -0,0 +1,14 @@
+print('Enter the value of n: ', end='')
+n= int(input())
+print('Enter ' +str(n)+ 'numbers:')
+numbers   = []
+i = 0
+while i<n:
+    numbers.insert(i, int(input()))
+    i = i+ 1
+sum= 0
+while i<n:
+    sum = sum+ numbers[i]
+    i= i+1
+avg = sum/n
+print('\nThe average value is: ', avg)
