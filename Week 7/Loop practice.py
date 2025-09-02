4295d4ce1eaa6911d980dae61dca2b5e128dc993
MD Huzaifa <hmd131021@gmail.com>
2025-09-02 23:58:20 +0600

    Update Loop practice.py

--- Week 7/Loop practice.py
+++ Week 7/Loop practice.py
@@ -1,3 +1,4 @@
+Taking so much input and then finding the summation using for loop.
 print('Enter the value of n: ', end='')
 n= int(input())
 print('Enter ' +str(n)+ 'numbers:')
@@ -9,3 +10,4 @@ for i in range(n):
     sum = sum+ numbers[i]
 avg = sum/n
 print('\nThe average value is: ', avg)
+
